2ce4f52b7307e5dfe155843c17396deab207b68f
myke_roly <myke_roly@hotmail.com>
2019-10-15 01:23:06 -0300

    subiendo web statica build

---
+++ precache-manifest.47a4a348dc7bb2c934942ff1b9c29f01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82bd7017e40e02a10e2462220a20da0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e20dd56891d3223e7a23",
+    "url": "/static/css/main.66fad31c.chunk.css"
+  },
+  {
+    "revision": "ff9f84697d329e35c822",
+    "url": "/static/js/2.5f4d1664.chunk.js"
+  },
+  {
+    "revision": "3b992144a2beba07aa40",
+    "url": "/static/js/3.eff7e49b.chunk.js"
+  },
+  {
+    "revision": "e20dd56891d3223e7a23",
+    "url": "/static/js/main.7a7538a6.chunk.js"
+  },
+  {
+    "revision": "68d08d6d447300927099",
+    "url": "/static/js/runtime-main.0de7c831.js"
+  }
+]);
